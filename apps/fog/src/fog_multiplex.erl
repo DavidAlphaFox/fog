a6694dc37df6bb1736aa938fb59fb2e5bb01935b
David.Gao <david.alpha.fox@gmail.com>
2014-07-29 20:47:04 +0800

    add fog_multiplex

--- apps/fog/src/fog_multiplex.erl
+++ apps/fog/src/fog_multiplex.erl
@@ -25,13 +25,17 @@
 	connected,
 	heart_beat,
 	miss,
-	socket
+	socket,
+	buff
 	}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
-
+to_princess(ID,Bin)->
+	gen_server:cast(?SERVER,{to_princess,ID,Bin}).
+fetch(Pid,ID,Address,Port)->
+	gen_server:call(?SERVER,{fetch,Pid,ID,Address,Port}).
 %%--------------------------------------------------------------------
 %% @doc
 %% Starts the server
@@ -67,8 +71,11 @@ init({IP,Port,HeartBeat}) ->
 		connected = false,
 		heart_beat = HeartBeat,
 		miss = 0,
-		socket = undefined
+		socket = undefined,
+		buff = <<>>
 	},
+	multiplex_monitor = ets:new(multiplex_monitor, [ordered_set, protected, named_table]),   
+	multiplex_mapper = ets:new(multiplex_mapper, [ordered_set, protected, named_table]),   
 	{ok,State,0}.
 
 %%--------------------------------------------------------------------
@@ -85,6 +92,20 @@ init({IP,Port,HeartBeat}) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_call({fetch,Pid,ID,Address,Port},_From,#state{heart_beat = HeartBeat,socket = Socket} = State)->
+	Addr = <<Address/binary>>,
+	AddrLen = erlang:byte_size(Addr), 
+	Data = <<AddrLen:32/big,Addr/binary,Port:16/big>>,
+	Packet = pack(ID,1,Data),
+	Result = ranch_ssl:send(Socket,Packet),
+	case Result of
+		ok ->
+			hm_misc:monitor(Pid,multiplex_monitor),
+			ets:insert(multiplex_mapper, {ID,Pid}),
+			{reply,ok,State,HeartBeat};
+		{error,Error}->
+			{reply,{error,Error},State,HeartBeat}
+	end;
 handle_call(_Request, _From, State) ->
 	Reply = ok,
 	{reply, Reply, State}.
@@ -99,6 +120,16 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast({to_princess,ID,Bin},#state{heart_beat = HeartBeat,socket = Socket} = State)->
+	Packet = pack(ID,2,Bin),
+	Result = ranch_ssl:send(Socket,Packet),
+	case Result of
+		ok ->
+			ok;
+		{error,_Error}->
+			remote_close(ID)
+	end,
+	{noreply, State,HeartBeat};
 handle_cast(_Msg, State) ->
 	{noreply, State}.
 
@@ -112,11 +143,26 @@ handle_cast(_Msg, State) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+
+handle_info({ssl, Socket, Bin},#state{heart_beat = HeartBeat,socket = Socket,buff = Buff} = State) ->
+  % Flow control: enable forwarding of next TCP message
+  ok = ranch_ssl:setopts(Socket, [{active, false}]),
+  {Packet,NewBuff} = unpack(<<Buff/bits,Bin/bits>>,[]),
+  StateBuff = State#state{buff = NewBuff},
+  NewState = packet(Packet,StateBuff),
+  ok = ranch_ssl:setopts(Socket, [{active, once}]),
+  {noreply,NewState,HeartBeat};
+
+handle_info({ssl_closed, Socket}, #state{socket = Socket} = State) ->
+  {stop, ssl_closed, State};
+
+
 handle_info(timeout,#state{ip = IP,port = Port,connected = false,heart_beat = HeartBeat} = State )->
 	lager:log(info,?MODULE,"Try to connect to ~s:~p~n",[IP,Port]),
 	Result = ranch_ssl:connect(IP,Port,[]),
 	NewState = case Result of
 		{ok,Socket}->
+			ok = ranch_ssl:setopts(Socket, [{active, once}]),
 			State#state{connected = true,socket = Socket};
 		{error,Error}->
 			lager:log(info,?MODULE,"Connect to ~s:~p fail. Reason: ~p~n",[IP,Port,Error]),
@@ -129,12 +175,26 @@ handle_info(timeout,#state{connected = true,heart_beat = HeartBeat,miss = Miss,s
 	NewState = if 
 			Miss > 2 ->
 				ranch_ssl:close(Socket),
+				loop_close(),
 				State#state{connected = false,miss = 0,socket = undefined};
 			true -> 
-				ranch_ssl:send(Socket,<<"ping">>),
+				Packet = pack(0,0,<<>>),
+				ranch_ssl:send(Socket,Packet),
 	 			State#state{miss = Miss + 1}
 	 		end,
 	{noreply,NewState,HeartBeat};
+
+handle_info({'DOWN', _MonitorRef, process, Pid, _Info},#state{socket = Socket} = State) -> 
+	hm_misc:demonitor(Pid,multiplex_monitor),
+	case ets:match_object(multiplex_mapper,{'_',Pid}) of
+		[] ->
+			{noreply,State};
+		[{ID,Pid}] ->
+			Close = pack(ID,3,<<>>),
+			ets:delete(multiplex_mapper,ID),
+			ranch_ssl:send(Socket,Close),
+  		{noreply, State}
+  end;
 
 handle_info(_Info, State) ->
 	{noreply, State}.
@@ -151,6 +211,7 @@ handle_info(_Info, State) ->
 %% @end
 %%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
+	lager:log(info,?MODULE,"stop"),
 	ok.
 
 %%--------------------------------------------------------------------
@@ -167,4 +228,65 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+loop_close()->
+	case ets:match_object(multiplex_mapper,{'_','_'}) of
+		[] ->
+			ok;
+		Any ->
+			loop_close(Any)
+  end.
+loop_close([])->
+	ok;
+loop_close([H|T])->
+	{ID,_Pid} = H,
+	remote_close(ID),
+	loop_close(T).
 
+remote_close(ID)->
+	case ets:match_object(multiplex_mapper,{ID,'_'}) of
+		[] ->
+			ok;
+		[{ID,Pid}] ->
+			ets:delete(multiplex_mapper,ID),
+			hm_misc:demonitor(Pid,multiplex_monitor),
+  		socks_protocol:close(Pid),
+  		ok
+  end.
+to_client(ID,Bin)->
+	case ets:match_object(multiplex_mapper,{ID,'_'}) of
+		[] ->
+			ok;
+		[{ID,Pid}] ->
+  		socks_protocol:to_client(Pid,Bin),
+  		ok
+  end.
+
+packet([],State)->
+	State;
+packet([<<0:64/integer,0:32/integer>>|T],State)->
+	Miss = State#state.miss,
+	NewState = State#state{miss = Miss - 1},
+	lager:log(info,?MODULE,"pong~n"),
+	packet(T,NewState);
+
+packet([<<ID:64/integer,2:32/integer,Rest/bits>>|T],State)->
+	lager:log(info,?MODULE,"free->->client~n"),
+	to_client(ID,Rest),
+	packet(T,State);
+packet([<<ID:64/integer,3:32/integer,_Rest/bits>>|T],State)->
+	lager:log(info,?MODULE,"free close~n"),
+	remote_close(ID),
+	packet(T,State).
+
+pack(ID,OP,Data)->
+	R1 = <<ID:64/integer,OP:32/integer,Data/bits>>,
+	Len = erlang:byte_size(R1),
+	<<Len:32/big,R1/bits>>.
+
+unpack(Data,Acc) when byte_size(Data) < 4 ->
+	{lists:reverse(Acc),Data};
+unpack(<<Len:32/big,PayLoad/bits>> = Data,Acc) when Len > byte_size(PayLoad) ->
+	{lists:reverse(Acc),Data};
+unpack(<<Len:32/big, _/bits >> = Data, Acc) ->                                                                                                                                                                                                                                                 
+  << _:32/big,Packet:Len/binary, Rest/bits >> = Data,
+  unpack(Rest,[Packet|Acc]).
